57256dbfbf5939798f59567e89e988f3d0570c41
ESLL3N <esllenescolar@gmail.com>
2022-10-03 16:45:52 +0000

    correção

---
+++ app/src/main/java/esllenlicurgo/repositories/AlunosRepository.java
@@ -0,0 +1,8 @@
+package esllenlicurgo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import esllenlicurgo.models.Aluno;
+
+public interface AlunosRepository extends CrudRepository<Aluno, Integer> {
+
+} 
