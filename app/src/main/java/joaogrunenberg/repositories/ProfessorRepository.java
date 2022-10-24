686af1e0a39c54e8e7f2d85f8bdfdd49ac4c97ef
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-10-24 17:48:33 +0000

    aceiteiaderrota.jsp

--- app/src/main/java/joaogrunenberg/repositories/ProfessorRepository.java
+++ app/src/main/java/joaogrunenberg/repositories/ProfessorRepository.java
@@ -3,6 +3,6 @@ package joaogrunenberg.repositories;
 import org.springframework.data.repository.CrudRepository;
 import joaogrunenberg.models.Professor;
 
-public class ProfessorRepository extends CrudRepository <Professor, Integer> {
+public class ProfessorRepository extends CrudRepository<Professor, Integer> {
     
 }
